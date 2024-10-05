f61a0fab7889268f74d63031beb7de13aa447ae3
Jason White <jmw5598@gmail.com>
2024-10-05 09:12:28 -0400

    chore: removes titlebar and makes window draggable by root window

--- stock_clock.py
+++ stock_clock.py
@@ -9,7 +9,6 @@ font_size=32
 font_color="lime"
 font_color_alt="red"
 background_color="black"
-
 chime.theme('chime')
 
 
@@ -18,7 +17,7 @@ def is_last_five_seconds(seconds):
 
 
 def is_five_seconds_before_five_minute(minutes, seconds):
-    return is_last_five_seconds(seconds) and (minutes + 1) % 5 == 0
+    return (55 < seconds <= 59) and (minutes + 1) % 5 == 0
 
 
 def should_change_font_color(seconds):
@@ -47,16 +46,45 @@ def update_time():
     clock_label.config(text=current_time)
     clock_label.after(1000, update_time) # Update every 1 second
 
+
+def on_show_context_menu(event):
+    menu = tk.Menu(root, tearoff=0)
+    menu.add_command(label="Settings", command=on_show_settings_window)
+    menu.add_command(label="Exit", command=root.destroy)
+
+    def close_context_menu(event):
+        if menu.winfo_exists():
+            menu.unpost()
+
+    root.bind("<Button-1>", close_context_menu)
+    menu.post(event.x_root, event.y_root)
+
+
+def on_show_settings_window():
+    print("Creating and showing settings window")
+
+
+def on_drag_window(event):
+    root.geometry(f"+{event.x_root}+{event.y_root}")
+
+
 # Set up the main application window
 root = tk.Tk()
 root.title("Stock Clock")
+root.bind("<Button-3>", on_show_context_menu)
+root.overrideredirect(True)
+
 
 # Create a label to display the time
 clock_label = tk.Label(root, font=("Helvetica", font_size), fg=font_color, bg=background_color)
-clock_label.pack(pady=0)
+clock_label.pack(pady=4, padx=4, expand=True)
+
 
 # Start the time update loop
 update_time()
 
+
 # Run the application
+root.bind("<B1-Motion>", on_drag_window)
+root.eval(f"tk::PlaceWindow {str(root)} center")
 root.mainloop()
